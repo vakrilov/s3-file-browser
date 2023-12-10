6dc2cb7272b74a39847a2e16cd50d40614122924
vakrilov <alexander.vakrilov@gmail.com>
2023-12-10 11:23:04 +0200

    working directory compnent

---
+++ src/components/WorkingDirectory.tsx
@@ -0,0 +1,10 @@
+import "./WorkingDirectory.scss"
+
+
+export const WorkingDirectory = () => {
+  return (
+    <div className="working-directory">
+      <h1>WorkingDirectory</h1>
+    </div>
+  )
+}
