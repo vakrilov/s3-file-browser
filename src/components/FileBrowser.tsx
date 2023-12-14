4c729f06def4cee4c59b071bed6b006d17a5ded5
vakrilov <alexander.vakrilov@gmail.com>
2023-12-14 22:03:35 +0200

    Logout button

--- src/components/FileBrowser.tsx
+++ src/components/FileBrowser.tsx
@@ -1,15 +1,30 @@
+import { useContext } from "react";
+import { VscSignOut } from "react-icons/vsc";
 import { TreeView } from "./TreeView/TreeView";
 import { WorkingDirectory } from "./WorkingDirectory/WorkingDirectory";
 
 import "./FileBrowser.scss";
+import { ApiClientContext } from "../api/context";
 
-export const FileBrowser = () => (
-  <div className="file-browser">
-    <div className="left-panel">
-      <TreeView />
+export const FileBrowser = () => {
+  const { client, clearClient } = useContext(ApiClientContext);
+
+  return (
+    <div className="file-browser">
+      <header className="header">
+        <h2>
+          Bucket: <strong>{client?.bucket}</strong>
+        </h2>
+        <button onClick={clearClient}>
+          <VscSignOut /> Logout
+        </button>
+      </header>
+      <div className="left-panel">
+        <TreeView />
+      </div>
+      <div className="right-panel">
+        <WorkingDirectory />
+      </div>
     </div>
-    <div className="right-panel">
-      <WorkingDirectory />
-    </div>
-  </div>
-);
+  );
+};
